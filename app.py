b9b7343ae4229a3cc04f87905b71d3febbdd5f9d
derek <derek@derekrgreene.com>
2025-03-20 01:14:21 +0000

    removed extra coffee

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, request, jsonify, session, redirect, url_for, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
