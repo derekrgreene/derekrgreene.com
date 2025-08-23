ee699ed8ac3a96ca7b4cb9bf3526a0e3f496738e
derekrgreene <derek@derekrgreene.com>
2025-08-22 19:43:27 -0700

    Updated gitignore

--- derekrgreene/config/prod.exs
+++ derekrgreene/config/prod.exs
@@ -20,5 +20,8 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Disable dev routes in production (enables admin dashboard)
+config :derekrgreene, :dev_routes, false
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
